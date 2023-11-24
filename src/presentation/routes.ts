cd04779297beeb53e05cb40aff734f22a871f8a2
tonovarela <tonovarela@live.com>
2023-11-24 10:08:06 -0600

    mod en routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -5,7 +5,8 @@ import { TodosRoutes } from "./controllers/todos/routes";
 export class AppRoutes {
 
     static get routes(): Router {
-        const router = Router();        
+        const router = Router();   
+             
         router.use("/api/todos",TodosRoutes.routes);
         return router;
     }
